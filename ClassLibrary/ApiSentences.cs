fb354a79f72a97c8031e5b22a0370d0a301c37a1
Mikael Larsson <s.mikael.larsson@gmail.com>
2024-10-18 09:16:59 +0200

    anropa API och få tillbaka mening

--- ClassLibrary/ApiSentences.cs
+++ ClassLibrary/ApiSentences.cs
@@ -2,6 +2,32 @@
 {
     public class ApiSentences
     {
+        //endast tillgänglig inom klassen, variabeln kan inte ändras i efterhand
+        private readonly HttpClient client;
 
+        public ApiSentences() 
+        {
+            client = new HttpClient();
+        }
+       
+        public async Task<string> sentencesFromApi(string url)
+        {
+            try
+            {
+                //skicka GET-förfrågan
+                HttpResponseMessage responseFromApi = await client.GetAsync(url);
+
+                //felhantering
+                responseFromApi.EnsureSuccessStatusCode();
+
+                string responseData = await responseFromApi.Content.ReadAsStringAsync();
+                return responseData;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Det uppstod ett fel när API:n anropades: {ex.Message}");
+                return string.Empty;
+            }
+        }
     }
 }
--- typeracer/Program.cs
+++ typeracer/Program.cs
@@ -1,3 +1,13 @@
 ﻿using ClassLibrary;
 
-Console.WriteLine("hej");
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        ApiSentences apiSentences = new ApiSentences();
+        string url = "https://api.kanye.rest";
+
+        string apiData = await apiSentences.sentencesFromApi(url);
+        Console.WriteLine(apiData);
+    }
+}
